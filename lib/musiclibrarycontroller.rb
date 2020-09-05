2c782cbb3507c1f9d736d6a836425f13bf86ce51
Christopher Arias <chrstphr.aaron@gmail.com>
2020-09-05 23:27:25 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -51,4 +51,9 @@ class MusicLibraryController
     artists = Artist.all.sort {|a,b| a.name <=> b.name}
     artists.each.with_index(1) {|artist, i| puts "#{i}. #{artist.name}"}
   end
+
+  def list_genres
+    genres = Genre.all.sort {|a,b| a.name <=> b.name}
+    genres.each.with_index(1) {|genre, i| puts "#{i}. #{genre.name}"}
+  end
 end
